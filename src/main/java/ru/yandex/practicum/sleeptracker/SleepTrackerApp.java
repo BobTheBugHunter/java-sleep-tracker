36c2ce5aff2c942490377254e0df762f4908561d
BobTheBugHunter <ser.sven@yandex.ru>
2026-03-19 19:22:56 +0500

    add SleepingSession and Functional(Class with Functions like cycle)

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,8 +1,32 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SleepTrackerApp {
+
+    private static final List<SleepingSession> logFromFile = new ArrayList<>();
+    private static final List<Functional> functions = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        readFile("sleep_log.txt");
+
+    }
+
+    public static void readFile(String fileName) {
+        try(BufferedReader br = new BufferedReader((new FileReader(fileName)))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                logFromFile.add(new  SleepingSession(line));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepingSession.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.sleeptracker;
+
+public class SleepingSession {
+    private final String session;
+    public SleepingSession(String session) {
+        this.session = session;
+    }
+
+    public String getSession() {
+        return session;
+    }
+}
