4e7d4e993ab8650f9e60016a0e91f55770649efc
BobTheBugHunter <ser.sven@yandex.ru>
2026-03-20 19:35:16 +0500

    delete unused import

--- src/main/java/only/calculation/functions/AverageSession.java
+++ src/main/java/only/calculation/functions/AverageSession.java
@@ -8,26 +8,30 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class AverageSession implements Function<List<SleepingSession>, Long> {
+
     private final List<Duration> durations = new ArrayList<>();
+
     @Override
     public Long apply(List<SleepingSession> sleepingSessions) {
 
         if (sleepingSessions.isEmpty()) {
+            if (durations.isEmpty()) {
+                return 0L;
+            }
             return (durations.stream().mapToLong(Duration::toMinutes).sum()) / durations.size();
         }
 
         DateTimeFormatter dateTimeFormatter  = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
-        List<String> s = sleepingSessions.stream()
+        List<String> sessionParts = sleepingSessions.stream()
                 .flatMap(sleepingSession -> Arrays.stream(sleepingSession.getSession().split(";")))
                 .collect(Collectors.toList());
 
-        String firstTime = s.getFirst();
-        String secondTime = s.get(1);
+        String firstTime = sessionParts.getFirst();
+        String secondTime = sessionParts.get(1);
         LocalDateTime dateTimeFirst = LocalDateTime.parse(firstTime, dateTimeFormatter);
         LocalDateTime dateTimeSecond = LocalDateTime.parse(secondTime, dateTimeFormatter);
         Duration duration = Duration.between(dateTimeFirst, dateTimeSecond);
